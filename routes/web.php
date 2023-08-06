b659e3c64825a2a44e1d5c2f59a0eb959151e102
mramadhanurh <mramadhanur@gmail.com>
2023-08-06 07:26:13 +0700

    layout dashboard

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('layouts.dashboard');
+});
+
 Auth::routes([
     'register' => false
 ]);
